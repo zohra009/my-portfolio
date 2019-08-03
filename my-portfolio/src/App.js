38ae11aa65b25738e4a2aac9afd5c46a80f13343
zor009 <anzohra@outlook.com>
2019-08-03 14:09:54 -0400

    delete useless files

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
+
 import './App.css';
 import Project from "./projects-data";
 
@@ -10,22 +10,9 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+     <div>
+       <h1>hello world</h1>
+     </div>
     );
   }
 }
