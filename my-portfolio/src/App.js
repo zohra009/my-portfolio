c0ee60e985aa1404e565e52377e5417efdcb75e4
zor009 <anzohra@outlook.com>
2019-08-13 23:21:20 -0400

    changes

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+// import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 // import * as ReactMDL from 'react-mdl';
 
 
@@ -11,29 +11,30 @@ class App extends Component {
 
   render() {
     return (
-      <div className="big-content">
-        <Layout>
-            <Header title="Hello World" scroll>
-                <Navigation>
-                    <a href="/">Link</a>
-                    <a href="/">Link</a>
-                    <a href="/">Link</a>
-                    <a href="/">Link</a>
-                </Navigation>
-            </Header>
-            <Drawer title="Title">
-                <Navigation>
-                    <a href="/">Link</a>
-                    <a href="/">Link</a>
-                    <a href="/">Link</a>
-                    <a href="/">Link</a>
-                </Navigation>
-            </Drawer>
-            <Content>
-                <div className="content" />
-            </Content>
-        </Layout>
-    </div>
+      <div>"hello world"</div>
+    //   <div className="big-content">
+    //     <Layout>
+    //         <Header title="Hello World" scroll>
+    //             <Navigation>
+    //                 <a href="/">Link</a>
+    //                 <a href="/">Link</a>
+    //                 <a href="/">Link</a>
+    //                 <a href="/">Link</a>
+    //             </Navigation>
+    //         </Header>
+    //         <Drawer title="Title">
+    //             <Navigation>
+    //                 <a href="/">Link</a>
+    //                 <a href="/">Link</a>
+    //                 <a href="/">Link</a>
+    //                 <a href="/">Link</a>
+    //             </Navigation>
+    //         </Drawer>
+    //         <Content>
+    //             <div className="content" />
+    //         </Content>
+    //     </Layout>
+    // </div>
     );
   }
 }
