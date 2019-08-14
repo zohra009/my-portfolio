8d2150e2e360775e9c811c6f2d85eaba4ad54bef
zor009 <anzohra@outlook.com>
2019-08-14 13:56:26 -0400

    mdl

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,40 +1,47 @@
 import React, { Component } from 'react';
 import './App.css';
-// import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
-// import * as ReactMDL from 'react-mdl';
+import "react-mdl/extra/material.js";
+import {
+  Drawer,
+  Navigation,
+  Header,
+  Textfield,
+  Layout,
+  Content
+} from "react-mdl";
 
 
 class App extends Component {
-  state = {
-    
-  }
+ componentDidMount() {
+
+ }
 
   render() {
     return (
-      <div>"hello world"</div>
-    //   <div className="big-content">
-    //     <Layout>
-    //         <Header title="Hello World" scroll>
-    //             <Navigation>
-    //                 <a href="/">Link</a>
-    //                 <a href="/">Link</a>
-    //                 <a href="/">Link</a>
-    //                 <a href="/">Link</a>
-    //             </Navigation>
-    //         </Header>
-    //         <Drawer title="Title">
-    //             <Navigation>
-    //                 <a href="/">Link</a>
-    //                 <a href="/">Link</a>
-    //                 <a href="/">Link</a>
-    //                 <a href="/">Link</a>
-    //             </Navigation>
-    //         </Drawer>
-    //         <Content>
-    //             <div className="content" />
-    //         </Content>
-    //     </Layout>
-    // </div>
+    
+      <div className="big-content">
+        <Layout>
+            <Header title="Hello World" scroll>
+                <Navigation>
+                    <a href="/">Link</a>
+                    <a href="/">Link</a>
+                    <a href="/">Link</a>
+                    <a href="/">Link</a>
+                </Navigation>
+            </Header>
+            <Drawer title="Title">
+                <Navigation>
+                    <a href="/">Link</a>
+                    <a href="/">Link</a>
+                    <a href="/">Link</a>
+                    <a href="/">Link</a>
+                </Navigation>
+            </Drawer>
+            <Content>
+                <div className="content" />
+            </Content>
+        </Layout>
+    </div>
     );
   }
 }
