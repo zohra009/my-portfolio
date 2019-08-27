2aa603ae292a040a5d80ceb4f20b1430f8f19563
zor009 <anzohra@outlook.com>
2019-08-26 21:56:47 -0400

    routes

--- my-portfolio/src/components/contact.js
+++ my-portfolio/src/components/contact.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 
 
 class Contact extends Component {
-    render () 
+    render () {
         return(
            <div><h1>Contact Page</h1></div> 
         );
--- my-portfolio/src/components/main.js
+++ my-portfolio/src/components/main.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import LandingPage from './landingpage';
+import AboutMe from './aboutme';
+import Projects from './projects';
+import Resume from './resume';
+import Contact from './contact';
+
+
 
 
 const Main = () => (
     <Switch>
         <Route exact path="/" component={LandingPage} />
+        <Route path="/aboutme" component={AboutMe} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/resume" component={Resume} />
+        <Route path="/contact" component={Contact} />
+        
     </Switch>
 );
 
