10ae3dd9a4edc55d6145f83245a9115146c42638
zor009 <anzohra@outlook.com>
2019-08-27 16:11:59 -0400

    replicate card & added div

--- my-portfolio/src/components/projects.js
+++ my-portfolio/src/components/projects.js
@@ -12,10 +12,29 @@ class Projects extends Component {
     toggleCategories() {
         if (this.state.activeTab === 0 ) {
             return (
-                <div>
+                <div className="projects-grid">
+                    {/* Project 1 */}
                     <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
                     <CardTitle style={{color: '#fff', height: '176px', background: 'url( https://media.istockphoto.com/photos/close-up-of-fast-food-snacks-and-drink-on-table-picture-id487077896?k=6&m=487077896&s=612x612&w=0&h=BQ-_Fv1sZ7_DvL4Jbto3j83efC2lvLoEdjByXy67n-Y=) center/ cover '}}>
                         React Project #1
+                    </CardTitle>
+                    <CardText>
+                        Looking on the go cheap food while enjoying a boozy Friyay with friends? You're in luck 
+                        thanks to DrunkFoodz App. Your favorite DrunkFoodz within thumbs reach. 
+                    </CardText>
+                    <CardActions border>
+                        <Button colored>Github</Button>
+                        <Button colored>Demo</Button>
+                    </CardActions>
+                    <CardMenu style={{color: '#fff'}}>
+                        <IconButton name='share' />
+                    </CardMenu>
+                    </Card>
+
+                        {/* Project 2 */}
+                    <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                    <CardTitle style={{color: '#fff', height: '176px', background: 'url( https://losangeles.cbslocal.com/wp-content/uploads/sites/14984641/2013/07/fast-food.jpg?w=625) center/ cover '}}>
+                        React Project #2
                     </CardTitle>
                     <CardText>
                         Looking on the go cheap food while enjoying a boozy Friyay with friends? You're in luck 
@@ -53,7 +72,7 @@ class Projects extends Component {
                     <Tab>Unity</Tab>
                 </Tabs>
                 
-                <Grid className="projects-grid">
+                <Grid>
                 <Cell col={12}>
                 <div className="content">{this.toggleCategories()}</div>
                 </Cell>
