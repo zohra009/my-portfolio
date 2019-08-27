2cb3c67d82f6e1f81d5d402d6c5e9c9bd291a0d5
zor009 <anzohra@outlook.com>
2019-08-26 21:45:39 -0400

    notation

--- my-portfolio/src/components/aboutme.js
+++ my-portfolio/src/components/aboutme.js
@@ -3,11 +3,11 @@ import React, {Component} from 'react';
 
 
 class About extends Component {
-    render (
+    render () {
         return(
            <div><h1>About Page</h1></div> 
-        )
-    )
+        );
+    }
 }
 
 export default About; 
--- my-portfolio/src/components/contact.js
+++ my-portfolio/src/components/contact.js
@@ -6,7 +6,7 @@ class Contact extends Component {
     render () 
         return(
            <div><h1>Contact Page</h1></div> 
-        )
+        );
     }
 }
 
