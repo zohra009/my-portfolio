85698067600bd519270f1a8fe3ae365031d17464
zor009 <anzohra@outlook.com>
2019-08-26 21:09:04 -0400

    main

--- my-portfolio/src/components/main.js
+++ my-portfolio/src/components/main.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import Landingpage from './landingpage';
 
+
 const Main = () => {
     <Switch>
         <Route exact path="/" component={LandingPage} />
     </Switch>
 }
+
+export default Main; 
