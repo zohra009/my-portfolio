f1d71763c0a129bcb3da1eec2c754bb1e1c7c754
zor009 <anzohra@outlook.com>
2019-08-26 20:44:01 -0400

    main

--- my-portfolio/src/index.js
+++ my-portfolio/src/index.js
@@ -6,7 +6,10 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
+import {BrowserRouter} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<BrowserRouter><App /></BrowserRouter>,
+ document.getElementById('root'));
 
 serviceWorker.unregister();
