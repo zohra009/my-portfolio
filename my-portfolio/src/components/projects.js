8110e7a801ede610f8be71a0132577799ca80674
zor009 <anzohra@outlook.com>
2019-08-26 21:46:17 -0400

    notation

--- my-portfolio/src/components/projects.js
+++ my-portfolio/src/components/projects.js
@@ -3,11 +3,11 @@ import React, {Component} from 'react';
 
 
 class Projects extends Component {
-    render (
+    render () {
         return(
            <div><h1>Projects Page</h1></div> 
-        )
-    )
+        );
+    }
 }
 
 export default Projects; 
