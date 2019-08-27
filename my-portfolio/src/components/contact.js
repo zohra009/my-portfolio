fc75928eb5b0d1eff5ac31793ac64c2458eabac6
zor009 <anzohra@outlook.com>
2019-08-27 16:46:53 -0400

    google fonts link

--- my-portfolio/src/components/contact.js
+++ my-portfolio/src/components/contact.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 
 
 class Contact extends Component {
     render () {
         return(
-           <div><h1>Contact Page</h1></div> 
+           <div className='contact-body'>
+               <Grid className="contact-grid">
+                <Cell col={6}> half page</Cell>
+                <Cell col={6}> half page</Cell>
+               </Grid>
+           </div> 
         );
     }
 }
