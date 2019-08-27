1d98d65369dfba0034548c05cba74f518ee77cb4
zor009 <anzohra@outlook.com>
2019-08-27 16:04:44 -0400

    delete section

--- my-portfolio/src/components/projects.js
+++ my-portfolio/src/components/projects.js
@@ -12,22 +12,24 @@ class Projects extends Component {
     toggleCategories() {
         if (this.state.activeTab === 0 ) {
             return (
-                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://image.shutterstock.com/image-photo/healthy-food-clean-eating-selection-260nw-722718097.jpg) center/ cover '}}>
-                    React Project #1
-                </CardTitle>
-                <CardText>
-                    Looking on the go cheap food while enjoying a boozy Friyay with friends? You're in luck 
-                    thanks to DrunkFoodz App. Your favorite DrunkFoodz within thumbs reach. 
-                </CardText>
-                <CardActions border>
-                    <Button colored>Github</Button>
-                    <Button colored>Demo</Button>
-                </CardActions>
-                <CardMenu style={{color: '#fff'}}>
-                    <IconButton name='share' />
-                </CardMenu>
-                </Card>
+                <div>
+                    <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                    <CardTitle style={{color: '#fff', height: '176px', background: 'url( https://media.istockphoto.com/photos/close-up-of-fast-food-snacks-and-drink-on-table-picture-id487077896?k=6&m=487077896&s=612x612&w=0&h=BQ-_Fv1sZ7_DvL4Jbto3j83efC2lvLoEdjByXy67n-Y=) center/ cover '}}>
+                        React Project #1
+                    </CardTitle>
+                    <CardText>
+                        Looking on the go cheap food while enjoying a boozy Friyay with friends? You're in luck 
+                        thanks to DrunkFoodz App. Your favorite DrunkFoodz within thumbs reach. 
+                    </CardText>
+                    <CardActions border>
+                        <Button colored>Github</Button>
+                        <Button colored>Demo</Button>
+                    </CardActions>
+                    <CardMenu style={{color: '#fff'}}>
+                        <IconButton name='share' />
+                    </CardMenu>
+                    </Card>
+                </div>
             )
         }else if (this.state.activeTab === 1) {
             return (<div><h1>this is html/css</h1></div>)
@@ -50,14 +52,14 @@ class Projects extends Component {
                     <Tab>Javascript</Tab>
                     <Tab>Unity</Tab>
                 </Tabs>
-                <section className="projects-grid">
+                
                 <Grid className="projects-grid">
                 <Cell col={12}>
                 <div className="content">{this.toggleCategories()}</div>
                 </Cell>
                 </Grid>
                 
-                </section>
+                
            </div> 
         );
     }
