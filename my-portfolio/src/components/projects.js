f2423866f86d050bb2cd3013dc220ed967424e2c
zor009 <anzohra@outlook.com>
2019-08-27 03:15:47 -0400

    togglecategories

--- my-portfolio/src/components/projects.js
+++ my-portfolio/src/components/projects.js
@@ -8,6 +8,23 @@ class Projects extends Component {
         super(props);
         this.state = { activeTab: 0 };
     }
+
+    toggleCategories() {
+        if (this.state.activeTab === 0 ) {
+            return (
+                <div><h1>this is react</h1></div>
+            )
+        }else if (this.state.activeTab === 1) {
+            return (<div><h1>this is html/css</h1></div>)
+        }else if (this.state.activeTab === 2) {
+            return (<div><h1>this is Ruby</h1></div>)
+        }else if (this.state.activeTab === 3) {
+            return (<div><h1>this is Javascript</h1></div>)
+        }else if (this.state.activeTab === 4) {
+            return (<div><h1>this is Unity</h1></div>)
+        }
+    }
+   
     render () {
         return(
            <div className="category-tabs">
@@ -17,9 +34,10 @@ class Projects extends Component {
                     <Tab>Ruby</Tab>
                     <Tab>Javascript</Tab>
                     <Tab>Unity</Tab>
-
-
                 </Tabs>
+                <section className="projects-grid">
+                {this.toggleCategories()}
+                </section>
            </div> 
         );
     }
