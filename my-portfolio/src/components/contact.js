9e7d96c27d3a1f44e511f079e2137798d91e4e0d
zor009 <anzohra@outlook.com>
2019-08-27 22:34:59 -0400

    font weight

--- my-portfolio/src/components/contact.js
+++ my-portfolio/src/components/contact.js
@@ -8,7 +8,17 @@ class Contact extends Component {
         return(
            <div className='contact-body'>
                <Grid className="contact-grid">
-                <Cell col={6}> half page</Cell>
+                <Cell col={6}> 
+                    <h1>Zohra Anwar </h1>
+                    <img 
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiwZzOUKSJxURYm5JiBw8_ytnE6PntWaMzoOyhSCvTgadacONl"
+                    alt="avatar"
+                    style={{height: '250px'}}
+                    />
+                    <p style={{width: '75%', margin: 'auto', paddingTop: '1em'}}>
+                        Thanks for visiting my portfolio!
+                    </p>
+                </Cell>
                 <Cell col={6}> half page</Cell>
                </Grid>
            </div> 
