89bebb988bd737983ffb3c9a10b44d5028387521
zor009 <anzohra@outlook.com>
2019-08-28 00:06:10 -0400

    list item content and icons

--- my-portfolio/src/components/contact.js
+++ my-portfolio/src/components/contact.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Grid, Cell } from 'react-mdl';
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
 
 
 
@@ -19,7 +19,37 @@ class Contact extends Component {
                         Thanks for visiting my portfolio!
                     </p>
                 </Cell>
-                <Cell col={6}> half page</Cell>
+                <Cell col={6}>
+                    <h2>Contact Me</h2>
+                    <hr/>
+
+                    <div className="contact-list">
+                        <List>
+                            <ListItem>
+                                <ListItemContent style={{fontSize: '23px', fontFamily: 'PT Serif'}}>
+                                    <i className="fa fa-envelope" aria-hidden="true" />
+                                    zohra.anwar@flatironschool.com
+                                </ListItemContent>
+                            </ListItem>
+
+                            <ListItem>
+                                <ListItemContent style={{fontSize: '23px', fontFamily: 'PT Serif'}}>
+                                    <i className="fa fa-skype" aria-hidden="true" />
+                                    MySkypeId
+                                </ListItemContent>
+                            </ListItem>
+                            
+                            <ListItem>
+                                <ListItemContent style={{fontSize: '23px', fontFamily: 'PT Serif'}}>
+                                    <i className="fa fa-phone-square" aria-hidden="true" />
+                                    (123)456-7896
+                                </ListItemContent>
+                            </ListItem> 
+                             
+                        </List>
+                    </div>
+
+                </Cell>
                </Grid>
            </div> 
         );
