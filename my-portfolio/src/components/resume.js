a232bfc887796efe1a9deed020e37ef47a48e20c
zor009 <anzohra@outlook.com>
2019-08-29 00:19:25 -0400

    h2

--- my-portfolio/src/components/resume.js
+++ my-portfolio/src/components/resume.js
@@ -9,13 +9,14 @@ class Resume extends Component {
            <div>
                <Grid>
                    <Cell col={4}>
-                    <div style={{textAlign: 'center'}}>
-                        <img 
-                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiwZzOUKSJxURYm5JiBw8_ytnE6PntWaMzoOyhSCvTgadacONl"
-                        alt='avatar'
-                        style={{height: '200px'}} 
-                        />
-                    </div>
+                        <div style={{textAlign: 'center'}}>
+                            <img 
+                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiwZzOUKSJxURYm5JiBw8_ytnE6PntWaMzoOyhSCvTgadacONl"
+                            alt='avatar'
+                            style={{height: '200px'}} 
+                            />
+                        </div>
+                        <h2 style={{paddingTop: '2em'}}>Zohra Anwar</h2>
                    </Cell>
                    <Cell className="resume-right-col" col={8}>Right Side</Cell>
 
