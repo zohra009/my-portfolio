1cb459511d2fca29ca9eb016f40e0c48f6e14f08
zor009 <anzohra@outlook.com>
2019-08-29 00:29:41 -0400

    p

--- my-portfolio/src/components/resume.js
+++ my-portfolio/src/components/resume.js
@@ -18,7 +18,15 @@ class Resume extends Component {
                         </div>
                         <h2 style={{paddingTop: '2em'}}>Zohra Anwar</h2>
                         <h4 style={{color: 'gray'}}>Full Stack Web Deveolper</h4>
-                        <hr style={{borderTop: '3px solid #833fb2'}}/>
+                        <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+                        <p> 
+                        Full Stack Web Developer from Flatiron Schools Immersive Coding Program with a biochemistry
+                        background. Develop practical responsive apps with optimal user experience. Have deep 
+                        knowledge of Ruby, Ruby on Rails, SQL, JQuery, JavaScript, React, Redux, CSS, HTML,
+                        Bootstrap, Semantic UI,Unity and C#. Discovered web development through a passion for 
+                        scientific research, data analytics, and teching. Inspired by creating innovative technology with a 
+                        focus on quality and simplicity. 
+                        </p>
                    </Cell>
                    <Cell className="resume-right-col" col={8}>Right Side</Cell>
 
