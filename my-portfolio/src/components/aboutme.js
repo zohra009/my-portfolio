889cfb7a23c005441d093fc97204f24f0defac8a
zor009 <anzohra@outlook.com>
2019-08-30 16:57:29 -0400

    change font

--- my-portfolio/src/components/aboutme.js
+++ my-portfolio/src/components/aboutme.js
@@ -5,17 +5,25 @@ import React, {Component} from 'react';
 class About extends Component {
     render () {
         return(
-           <div className="aboutme-content">
-               <p> 
-                Leveraging analytics and problem-solving skills acquired as a biochemist with software
-                engineering training, I look to cross the chasm between technology and practical application. 
-                Technically proficient in Ruby on Rails, JavaScript, React and a variety of other programming 
-                languages, I am seeking Full Stack Developer opportunities to create applications to improve 
-                our overall quality of life. As a graduate of the Flatiron School Software Engineering Immersive 
-                program and former college lecturer, I have further developed the ability to communicate 
-                technical applications effectively.
+            <div style={{textAlign: 'center', paddingTop: '5em'}}>
+                <img 
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiwZzOUKSJxURYm5JiBw8_ytnE6PntWaMzoOyhSCvTgadacONl"
+                alt='avatar'
+                style={{height: '200px'}} 
+                />
+           
+           
+               <p style={{width: '50%', margin: 'auto', paddingTop: '2em', fontSize: '18px', fontFamily: 'Roboto Condensed'}}> 
+                Leveraging analytics and problem-solving skills acquired as a biochemist with 
+                software engineering training, I look to cross the chasm between technology 
+                and practical application. Technically proficient in Ruby on Rails, JavaScript, 
+                React and a variety of other programming languages, I am seeking 
+                Full Stack Developer opportunities to create applications to improve 
+                our overall quality of life. As a graduate of the Flatiron School Software Engineering 
+                Immersive program and former college lecturer, I have further developed the 
+                ability to communicate technical applications effectively.
               </p>
-           </div> 
+            </div>
         );
     }
 }
