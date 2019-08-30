3c384797e5cb2f10e20f67c093794e292692f5be
zor009 <anzohra@outlook.com>
2019-08-30 15:25:37 -0400

    skills content

---
+++ my-portfolio/src/components/skills.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
+
+class Skill extends Component {
+    render () {
+        return (
+            <div>
+                <Grid>
+                    <Cell col={12}> 
+                    mycol
+                     </Cell>                 
+                </Grid>
+            </div>
+        )
+    }
+}
+
+export default Skill;
