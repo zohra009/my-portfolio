28e7d6425b831a1c82f56206e15e6efb617ad8bb
zor009 <anzohra@outlook.com>
2019-10-14 15:50:03 -0400

    Updates

---
+++ precache-manifest.4e4dc86c32ae7a9fbb3c071625be615c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2d15970f74596a69ca7",
+    "url": "/my-portfolio/static/js/runtime~main.674e5ed7.js"
+  },
+  {
+    "revision": "4bc160e4ce22e300cae5",
+    "url": "/my-portfolio/static/js/main.14072ab6.chunk.js"
+  },
+  {
+    "revision": "6fc9286afff0295647a5",
+    "url": "/my-portfolio/static/js/2.b6dff942.chunk.js"
+  },
+  {
+    "revision": "4bc160e4ce22e300cae5",
+    "url": "/my-portfolio/static/css/main.e8c020e4.chunk.css"
+  },
+  {
+    "revision": "6fc9286afff0295647a5",
+    "url": "/my-portfolio/static/css/2.3631032e.chunk.css"
+  },
+  {
+    "revision": "7f20b5974b836330bd501b70792ff3fb",
+    "url": "/my-portfolio/index.html"
+  }
+];
