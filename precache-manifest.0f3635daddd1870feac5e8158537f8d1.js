b6d632aa8440253d6f67b4d39dcf8d6afcb9da40
zor009 <anzohra@outlook.com>
2019-10-14 14:40:23 -0400

    Updates

--- precache-manifest.0f3635daddd1870feac5e8158537f8d1.js
+++ precache-manifest.0f3635daddd1870feac5e8158537f8d1.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.a8a9905a.js"
   },
   {
-    "revision": "393c28bb3cf45b7171be",
-    "url": "/static/js/main.4eee3c75.chunk.js"
+    "revision": "8d9dab45faeafc69c22b",
+    "url": "/static/js/main.8cefefca.chunk.js"
   },
   {
     "revision": "6fc9286afff0295647a5",
     "url": "/static/js/2.b6dff942.chunk.js"
   },
   {
-    "revision": "393c28bb3cf45b7171be",
-    "url": "/static/css/main.8268cf85.chunk.css"
+    "revision": "8d9dab45faeafc69c22b",
+    "url": "/static/css/main.e8c020e4.chunk.css"
   },
   {
     "revision": "6fc9286afff0295647a5",
     "url": "/static/css/2.3631032e.chunk.css"
   },
   {
-    "revision": "302b2228321b3710986516d7590cc362",
+    "revision": "642a348a86cc03dbba00559c8537dbf7",
     "url": "/index.html"
   }
 ];
