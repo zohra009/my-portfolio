423f294adf6e60152b8f0f9be321468080bc890f
zor009 <anzohra@outlook.com>
2019-10-28 23:57:16 -0400

    bio

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -16,10 +16,9 @@ class About extends Component {
                     />          
                 <p > 
                     Leveraging analytics and problem-solving skills acquired as a biochemist with 
-                    software engineering training, I look to cross the chasm between technology 
-                    and practical application. Technically proficient in Ruby on Rails, JavaScript, 
-                    React and a variety of other programming languages, I am seeking 
-                    Full Stack Developer opportunities to create applications to improve 
+                    software engineering. Technically proficient in Ruby on Rails, JavaScript, 
+                    React and a variety of other programming languages. I am seeking 
+                    Web Developer opportunities to create applications to improve 
                     our overall quality of life. As a graduate of the Flatiron School Software Engineering 
                     Immersive program and former college lecturer, I have further developed the 
                     ability to communicate technical applications effectively.
